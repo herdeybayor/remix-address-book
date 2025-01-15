7262b3e6afd04ef353bf9cfeb6ee7723d9b81c8e
herdeybayor <shereefdeen4real@gmail.com>
2025-01-15 02:59:57 +0100

    Add destroy contact route and action for contact deletion

--- app/routes.ts
+++ app/routes.ts
@@ -8,6 +8,7 @@ export default [
         index("routes/home.tsx"),
         route("contacts/:contactId", "routes/contact.tsx"),
         route("contacts/:contactId/edit", "routes/edit-contact.tsx"),
+        route("contacts/:contactId/destroy", "routes/destroy-contact.tsx"),
     ]),
     route("about", "routes/about.tsx"),
 ] satisfies RouteConfig;
